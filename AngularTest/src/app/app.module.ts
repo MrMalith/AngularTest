6df2ddd18511be0caebfc277dac8c1db34087f85
MrMalith <sachinthamalith047@gmail.com>
2020-04-13 22:29:30 +0530

    added cart service to product details

--- AngularTest/src/app/app.module.ts
+++ AngularTest/src/app/app.module.ts
@@ -8,13 +8,17 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +27,7 @@ import { RouterModule } from '@angular/router';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ])
   ],
   providers: [],
